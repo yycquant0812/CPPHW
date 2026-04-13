966127485e3ca9f8bc1d5fc8eec17bf0a81b2b34
yycquant0812 <xherobruce@gmail.com>
2026-04-13 09:59:37 +0800

    Add variable.cpp to display age and name

---
+++ variable.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+	int age = 38;
+	string name = "Bruce";
+	cout << "年齡:" << age << "姓名:" << name << endl; 
+	return 0;
+}
