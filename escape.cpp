69ecce03743da14c5d7dca01b57ec666fdc2733f
yycquant0812 <xherobruce@gmail.com>
2026-04-22 15:15:57 +0800

    Add escape.cpp for printing formatted strings

---
+++ escape.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main (int argc, char *argv[]) {
+	cout << "1快樂星球" << endl; 
+	cout << "2快樂 星球" << endl; 
+	cout << "3快樂\t星球" << endl; 
+	cout << "4快樂\n星球" << endl; 
+	cout << "\\5快樂星球\\" << endl; 
+	cout << "\"6快樂星球\"" << endl; 
+
+  	return 0;
+}
