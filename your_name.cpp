114dbcadba9b593ffe0d10437d7a394fd6f6aa27
yycquant0812 <xherobruce@gmail.com>
2026-04-27 09:51:50 +0800

    Add basic C++ program to greet user and input numbers

---
+++ your_name.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string name;
+    int a, b;
+    cout << "輸入姓名\n";
+    cin >> name;
+    cout << "hello, " << name <<endl;
+    cout << "輸入a空格b\n";
+    cin >> a >> b;
+    cout << "a是" << a << "b是" << b; 
+  	return 0;
+}
