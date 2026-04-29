27b6d7cb3093c0f812f603ef97722a2dd38798fd
yycquant0812 <xherobruce@gmail.com>
2026-04-29 15:01:39 +0800

    Add basic arithmetic operations in caculate.cpp

---
+++ caculate.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x1 = 5 + 3;
+    int x2 = 5 - 3;
+    int x3 = 5 * 3;
+    int x4 = 5 / 3;
+    float x5 = 5 / 3.0;
+    int x6 = 5 % 3;
+    
+    cout << "x1 (5+3)  = " << x1 << endl;
+    cout << "x2 (5-3)  = " << x2 << endl;
+    cout << "x3 (5*3)  = " << x3 << endl;
+    cout << "x4 (5/3)  = " << x4 << endl;
+    cout << "x5 (5/3.0)= " << x5 << endl;
+    cout << "x6 (5%3)  = " << x6 << endl;
+
+    return 0;
+}
