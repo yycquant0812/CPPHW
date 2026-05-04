dadfbb849cbfe3f5eb6cc190cccd7100fb36e3c9
yycquant0812 <xherobruce@gmail.com>
2026-05-04 09:40:34 +0800

    Add logic.cpp with basic comparison operations

---
+++ logic.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 8;
+    int b = 3;
+    int c = a - b;
+    cout << (a < b) << " ";
+    cout << (a >= b) << " ";
+    cout << (a >= c) << " ";
+    cout << (b < c) << " ";
+    cout << (b == b) << " ";
+    cout << (a != c) << " ";
+    
+
+    return 0;
+}
